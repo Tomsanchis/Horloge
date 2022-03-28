19781e0e26eac6fecc16d27ac58d8e5ccc2a9199
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-28 15:45:10 +0200

    add queryselector for divs

--- js/app.js
+++ js/app.js
@@ -2,6 +2,20 @@ let minutes = 24;
 let secondes = 60;
 
 
+const hourtop = document.querySelector('.hourtop');
+const hourtop2 = document.querySelector('.hourtop2');
+
+const hourbottom = document.querySelector('.hourbottom');
+const hourbottom2 = document.querySelector('.hourbottom2');
+
+
+const minutebottom = document.querySelector('.minutebottom');
+const minutebottom2 = document.querySelector('.minutebottom2');
+
+const minutetop = document.querySelector('.minutetop');
+const minutetop2 = document.querySelector('.minutetop2');
+
+
 function timer () {
     if (secondes > 0) {
     secondes--;
