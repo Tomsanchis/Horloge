aa4ebf297c876db0d1b7683126a31827c8197966
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-28 15:39:04 +0200

    add secondes minutes, function timer

--- js/app.js
+++ js/app.js
@@ -1,31 +1,17 @@
-const hourtop = document.querySelector('.hourtop');
-const hourbottom = document.querySelector('.hourbottom');
-
-const minutetop = document.querySelector('.minutetop');
-const minutebottom = document.querySelector('.minutebottom');
+let minutes = 24;
+let secondes = 60;
 
 
-let count = Date.now();
-
-setInterval(function () {
-    let instant = Date.now();
-    let reverse = instant - count;
-    const hours = Math.floor((reverse % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((reverse % (1000 * 60 * 60)) / (1000 * 60));
-    console.log(hours + "h " + minutes + "m ")
-    if (minutes == 1) {
-       minutetop.style.borderLeft = "none"
-       minutetop.style.borderTop = "none"
-       minutetop.style.borderBottom = "none"
-       minutebottom.style.borderLeft = "none"
-       minutebottom.style.borderTop = "none"
-       minutebottom.style.borderBottom = "none"
-    } else {
-        minutetop.style.border = "solid 5px rgb(174, 0, 255)"
-        minutetop.style.border = "solid 5px rgb(174, 0, 255)"
-        minutetop.style.border = "solid 5px rgb(174, 0, 255)"
-        minutebottom.style.border = "solid 5px rgb(174, 0, 255)"
-        minutebottom.style.border = "solid 5px rgb(174, 0, 255)"
-        minutebottom.style.border = "solid 5px rgb(174, 0, 255)"
+function timer () {
+    if (secondes > 0) {
+    secondes--;
     }
-}, 1000)
+    else if (secondes === 0) {
+        minutes--;
+        secondes = 60;
+    }
+
+    console.log(minutes + " : " + secondes)
+}
+
+setInterval(timer, 1000);
