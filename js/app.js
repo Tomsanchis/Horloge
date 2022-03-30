5dc1334600b07228f10959211a50ea2351a9a3ff
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-30 14:13:44 +0200

    clear interval ok

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
-let minutes = 24;
-let secondes = 60;
+let minutes = 1;
+let secondes = 1;
 
+const hourtop = document.querySelector(".hourtop");
+const hourtop2 = document.querySelector(".hourtop2");
 
-const hourtop = document.querySelector('.hourtop');
-const hourtop2 = document.querySelector('.hourtop2');
+const hourbottom = document.querySelector(".hourbottom");
+const hourbottom2 = document.querySelector(".hourbottom2");
 
-const hourbottom = document.querySelector('.hourbottom');
-const hourbottom2 = document.querySelector('.hourbottom2');
+const minutebottom = document.querySelector(".minutebottom");
+const minutebottom2 = document.querySelector(".minutebottom2");
 
+const minutetop = document.querySelector(".minutetop");
+const minutetop2 = document.querySelector(".minutetop2");
 
-const minutebottom = document.querySelector('.minutebottom');
-const minutebottom2 = document.querySelector('.minutebottom2');
-
-const minutetop = document.querySelector('.minutetop');
-const minutetop2 = document.querySelector('.minutetop2');
-
-
-function timer () {
-    if (secondes > 0) {
+function timer() {
+  let s = minutes + " : " + secondes;
+  if (secondes > 0) {
     secondes--;
-    }
-    else if (secondes === 0) {
-        minutes--;
-        secondes = 60;
-    }
-
-    console.log(minutes + " : " + secondes)
+  } else if (secondes === 0) {
+    minutes--;
+    secondes = 1;
+  }
+  if (s[0] === "0" && s[4] === "0") {
+    clearInterval(timerinterval);
+  }
+  console.log(s);
 }
 
-setInterval(timer, 1000);
+const timerinterval = setInterval(timer, 1000);
