3e99c9782eca01151d6eaafae893a06b1fa05d73
Tom <tsanchis@edenschool.fr>
2022-04-24 15:36:04 +0200

    horloge finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-let minutes = 1;
-let secondes = 1;
+let divs = document.querySelectorAll(".huit");
 
 const hourtop = document.querySelector(".hourtop");
 const hourtop2 = document.querySelector(".hourtop2");
@@ -13,18 +12,155 @@ const minutebottom2 = document.querySelector(".minutebottom2");
 const minutetop = document.querySelector(".minutetop");
 const minutetop2 = document.querySelector(".minutetop2");
 
-function timer() {
-  let s = minutes + " : " + secondes;
+let minutes = 0;
+let secondes = 5;
+
+function sr() {
   if (secondes > 0) {
     secondes--;
   } else if (secondes === 0) {
     minutes--;
-    secondes = 1;
+    secondes = 60;
   }
-  if (s[0] === "0" && s[4] === "0") {
-    clearInterval(timerinterval);
+  let timer = "";
+  if (minutes < 10) {
+    timer += "0" + minutes;
+  } else {
+    timer += minutes;
   }
-  console.log(s);
+  if (secondes < 10) {
+    timer += "0" + secondes;
+  } else {
+    timer += secondes;
+  }
+  if (
+    timer[0] === "0" &&
+    timer[1] === "0" &&
+    timer[2] === "0" &&
+    timer[3] === "0"
+  ) {
+    console.log("clear");
+    clearInterval(srinterval);
+  }
+  for (let i = 0; i < timer.length; i++) {
+    if (timer[i] === "0") {
+      console.log("0");
+      divs[i].firstElementChild.classList.add("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.add("droite");
+
+      divs[i].lastElementChild.classList.remove("haut");
+      divs[i].lastElementChild.classList.add("gauche");
+      divs[i].lastElementChild.classList.add("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "1") {
+      console.log("1");
+      divs[i].firstElementChild.classList.remove("gauche");
+      divs[i].firstElementChild.classList.remove("haut");
+      divs[i].firstElementChild.classList.add("droite");
+
+      divs[i].lastElementChild.classList.remove("haut");
+      divs[i].lastElementChild.classList.remove("gauche");
+      divs[i].lastElementChild.classList.remove("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "2") {
+      console.log("2");
+      divs[i].firstElementChild.classList.remove("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.add("droite");
+
+      divs[i].lastElementChild.classList.add("haut");
+      divs[i].lastElementChild.classList.add("gauche");
+      divs[i].lastElementChild.classList.add("bas");
+      divs[i].lastElementChild.classList.remove("droite");
+    }
+    if (timer[i] === "3") {
+      console.log("3");
+      divs[i].firstElementChild.classList.remove("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.add("droite");
+      divs[i].lastElementChild.classList.add("bas");
+
+      divs[i].lastElementChild.classList.add("haut");
+      divs[i].lastElementChild.classList.remove("gauche");
+      divs[i].lastElementChild.classList.add("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "4") {
+      console.log("4");
+      divs[i].firstElementChild.classList.add("gauche");
+      divs[i].firstElementChild.classList.remove("haut");
+      divs[i].firstElementChild.classList.add("droite");
+      divs[i].lastElementChild.classList.add("bas");
+
+      divs[i].lastElementChild.classList.add("haut");
+      divs[i].lastElementChild.classList.remove("gauche");
+      divs[i].lastElementChild.classList.remove("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "5") {
+      console.log("5");
+      divs[i].firstElementChild.classList.add("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.remove("droite");
+      divs[i].lastElementChild.classList.add("bas");
+
+      divs[i].lastElementChild.classList.add("haut");
+      divs[i].lastElementChild.classList.remove("gauche");
+      divs[i].lastElementChild.classList.add("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "6") {
+      console.log("6");
+      divs[i].firstElementChild.classList.add("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.remove("droite");
+      divs[i].lastElementChild.classList.remove("bas");
+
+      divs[i].lastElementChild.classList.add("haut");
+      divs[i].lastElementChild.classList.add("gauche");
+      divs[i].lastElementChild.classList.add("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "7") {
+      console.log("7");
+      divs[i].firstElementChild.classList.remove("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.add("droite");
+      divs[i].lastElementChild.classList.remove("bas");
+
+      divs[i].lastElementChild.classList.remove("haut");
+      divs[i].lastElementChild.classList.remove("gauche");
+      divs[i].lastElementChild.classList.remove("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "8") {
+      console.log("8");
+      divs[i].firstElementChild.classList.add("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.add("droite");
+      divs[i].lastElementChild.classList.add("bas");
+
+      divs[i].lastElementChild.classList.add("haut");
+      divs[i].lastElementChild.classList.add("gauche");
+      divs[i].lastElementChild.classList.add("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+    if (timer[i] === "9") {
+      console.log("9");
+      divs[i].firstElementChild.classList.add("gauche");
+      divs[i].firstElementChild.classList.add("haut");
+      divs[i].firstElementChild.classList.add("droite");
+      divs[i].lastElementChild.classList.add("bas");
+
+      divs[i].lastElementChild.classList.add("haut");
+      divs[i].lastElementChild.classList.remove("gauche");
+      divs[i].lastElementChild.classList.add("bas");
+      divs[i].lastElementChild.classList.add("droite");
+    }
+  }
 }
 
-const timerinterval = setInterval(timer, 1000);
+const srinterval = setInterval(sr, 1000);
